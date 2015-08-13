7da9df9e68cb7059e1af35bc75249cc6edc3eb61
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-08-13 07:28:58 -0400

    Relying on `es6-promise` for server side due to V8's overhead with `Arrays` for `Promises` (aka it's slower)

--- src/intro.js
+++ src/intro.js
@@ -1,11 +1,12 @@
 "use strict";
 
 (function (global) {
-const Promise = global.Promise || require("es6-promise").Promise;
-const Map = global.Map || require("es6-map");
-const Set = global.Set || require("es6-set");
-const fetch = global.fetch || require("node-fetch");
-const tuple = global.tuple || require("tiny-tuple");
+const server = process !== undefined && typeof process.nextTick === "function";
+const Promise = !server ? global.Promise : require("es6-promise").Promise;
+const Map = !server ? global.Map : require("es6-map");
+const Set = !server ? global.Set : require("es6-set");
+const fetch = !server ? global.fetch : require("node-fetch");
+const tuple = !server ? global.tuple : require("tiny-tuple");
 const r = [8, 9, "a", "b"];
 const regex = {
 	querystring: /\?.*/,
