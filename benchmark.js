2b0ffcd6367ac75e3b08443c64266499cb817b60
avoidwork <jason.mulligan@avoidwork.com>
2015-09-19 12:14:14 -0400

    Updating benchmark to compare the override timing

--- benchmark.js
+++ benchmark.js
@@ -1,48 +1,71 @@
 var haro = require('./lib/haro'),
     precise = require('precise'),
-    store = haro(null, {key: "id", index: ["name"]}),
+    Promise = require('es6-promise').Promise,
     i = -1,
     nth = 15000,
     data = [],
-    timer;
+    indexes = {};
 
 while (++i < nth) {
-  data.push({id: i, name: "abba " + i});
+    data.push({id: i, name: "abba " + i});
+    indexes[i] = "abba" + i;
 }
 
-timer = precise().start();
-store.batch(data, "set").then(function() {
-  timer.stop();
-  console.log("time to load data: "+(timer.diff()/1000000)+"ms");
-  console.log("datastore record count: " + store.total);
-  console.log("name indexes: " + store.indexes.get("name").size);
-}).then(function () {
-  var i = -1,
-      nth = 5;
+function second () {
+    var timer = precise().start(),
+        store = haro(null, {key: "id", index: ["name"]}),
+        deferreds = [];
 
-  console.log("testing time to 'find()' a record (first one is cold):");
+    deferreds.push(store.override(data, "records"));
+    deferreds.push(store.override(indexes, "indexes"));
 
-  while (++i < nth) {
-    (function () {
-  var timer2 = precise().start();
-  var record = store.find({name: 'abba 12345'});
-  timer2.stop();
-  console.log((timer2.diff()/1000000)+"ms");
-    }());
-  }
-}).then(function () {
-  var i = -1,
-      nth = 5;
+    Promise.all(deferreds).then(function () {
+        timer.stop();
+        console.log("time to override data: " + (timer.diff() / 1000000) + "ms");
+    });
+}
 
-  console.log("testing time to 'search(regex, index)' for a record (first one is cold):");
+function first () {
+    var timer = precise().start(),
+        store = haro(null, {key: "id", index: ["name"]});
 
-  while (++i < nth) {
-    (function () {
-  var timer2 = precise().start();
-  var record = store.search(/abba 12345/, 'name');
-  timer2.stop();
-  console.log((timer2.diff()/1000000)+"ms");
-    }());
-  }
-});
+    store.batch(data, "set").then(function () {
+        timer.stop();
+        console.log("time to batch insert data: " + (timer.diff() / 1000000) + "ms");
+        console.log("datastore record count: " + store.total);
+        console.log("name indexes: " + store.indexes.get("name").size);
+    }).then(function () {
+        var i = -1,
+            nth = 5;
+
+        console.log("testing time to 'find()' a record (first one is cold):");
+
+        while (++i < nth) {
+            (function () {
+                var timer2 = precise().start();
+                var record = store.find({name: 'abba 12345'});
+                timer2.stop();
+                console.log((timer2.diff() / 1000000) + "ms");
+            }());
+        }
+    }).then(function () {
+        var i = -1,
+            nth = 5;
+
+        console.log("testing time to 'search(regex, index)' for a record (first one is cold):");
+
+        while (++i < nth) {
+            (function () {
+                var timer2 = precise().start();
+                var record = store.search(/abba 12345/, 'name');
+                timer2.stop();
+                console.log((timer2.diff() / 1000000) + "ms");
+            }());
+        }
+
+        second();
+    });
+}
+
+first();
 
