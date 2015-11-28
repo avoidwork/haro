ecad29412424bd46b709512d87767d764c22d39c
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-11-28 15:25:27 -0500

    Fixing AMD detection

--- src/outro.js
+++ src/outro.js
@@ -1,7 +1,7 @@
 // Node, AMD & window supported
 if (typeof exports !== "undefined") {
 	module.exports = factory;
-} else if (typeof define === "function") {
+} else if (typeof define === "function" && define.amd) {
 	define(function () {
 		return factory;
 	});
