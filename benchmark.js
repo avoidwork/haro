8e8cd5189971a997ef4f0e4a36d552823b2e0d20
Jason Mulligan <jason.mulligan@avoidwork.com>
2017-03-18 13:42:53 -0400

    Adding a reasonable data set for benchmarking

--- benchmark.js
+++ benchmark.js
@@ -1,97 +1,90 @@
 const path = require("path"),
-    haro = require(path.join(__dirname, "lib", "haro.es6")),
-    precise = require("precise"),
-    nth = 15000,
-    data = [];
+	haro = require(path.join(__dirname, "lib", "haro.es6")),
+	precise = require("precise"),
+	data = require(path.join(__dirname, "data.json"));
 
 let indexes;
 
-let i = -1;
-
-while (++i < nth) {
-    data.push({id: i, name: "abba " + i});
-}
-
 function second () {
-    const timer = precise().start(),
-        store = haro(null, {key: "id", index: ["name"]}),
-        deferreds = [];
+	const timer = precise().start(),
+		store = haro(null, {key: "id", index: ["name"]}),
+		deferreds = [];
 
-    deferreds.push(store.override(data, "records"));
-    deferreds.push(store.override(indexes, "indexes"));
+	deferreds.push(store.override(data, "records"));
+	deferreds.push(store.override(indexes, "indexes"));
 
-    Promise.all(deferreds).then(function () {
-	    let i = -1,
-		    nth = 5;
+	Promise.all(deferreds).then(function () {
+		let i = -1,
+			nth = 5;
 
-        timer.stop();
-        console.log("time to override data: " + (timer.diff() / 1000000) + "ms");
-	    console.log("testing time to 'search(regex, index)' on overridden data for a record (first one is cold):");
+		timer.stop();
+		console.log("time to override data: " + (timer.diff() / 1000000) + "ms");
+		console.log("testing time to 'search(regex, index)' on overridden data for a record (first one is cold):");
 
-	    while (++i < nth) {
-		    (function () {
-			    const timer2 = precise().start();
-			    const record = store.search(/abba 2342/, "name");
-			    timer2.stop();
-			    console.log((timer2.diff() / 1000000) + "ms");
+		while (++i < nth) {
+			(function () {
+				const timer2 = precise().start();
+				const record = store.search(/Carly Conway/, "name");
+				timer2.stop();
+				console.log((timer2.diff() / 1000000) + "ms");
 
-			    if (!record) {
-				    console.log("Couldn't find record");
-                }
-		    }());
-	    }
-    });
+				if (!record) {
+					console.log("Couldn't find record");
+				}
+			}());
+		}
+	});
 }
 
 function first () {
 	const timer = precise().start(),
-        store = haro(null, {key: "id", index: ["name"]});
+		store = haro(null, {key: "id", index: ["name"]});
 
-    store.batch(data, "set").then(function () {
-        timer.stop();
-        console.log("time to batch insert data: " + (timer.diff() / 1000000) + "ms");
-        console.log("datastore record count: " + store.total);
-        console.log("name indexes: " + store.indexes.get("name").size);
-    }).then(function () {
-        let i = -1,
-            nth = 5;
+	store.batch(data, "set").then(function () {
+		timer.stop();
+		console.log("time to batch insert data: " + (timer.diff() / 1000000) + "ms");
+		console.log("datastore record count: " + store.total);
+		console.log("name indexes: " + store.indexes.get("name").size);
+	}).then(function () {
+		let i = -1,
+			nth = 5;
 
-        console.log("testing time to 'find()' a record (first one is cold):");
-        indexes = store.dump("indexes");
+		console.log("testing time to 'find()' a record (first one is cold):");
+		indexes = store.dump("indexes");
 
-        while (++i < nth) {
-            (function () {
-                const timer2 = precise().start();
-				const record = store.find({name: "abba 12345"});
-                timer2.stop();
-                console.log((timer2.diff() / 1000000) + "ms");
+		while (++i < nth) {
+			(function () {
+				const timer2 = precise().start();
+				const record = store.find({name: "Muriel Osborne"});
+				timer2.stop();
+				console.log((timer2.diff() / 1000000) + "ms");
 
-	            if (!record) {
-		            console.log("Couldn't find record");
-	            }
-            }());
-        }
-    }).then(function () {
-        let i = -1,
-            nth = 5;
+				if (!record) {
+					console.log("Couldn't find record");
+				}
+			}());
+		}
+	}).then(function () {
+		let i = -1,
+			nth = 5;
 
-        console.log("testing time to 'search(regex, index)' for a record (first one is cold):");
+		console.log("testing time to 'search(regex, index)' for a record (first one is cold):");
 
-        while (++i < nth) {
-            (function () {
-                const timer2 = precise().start();
-                const record = store.search(/abba 12345/, "name");
-                timer2.stop();
-                console.log((timer2.diff() / 1000000) + "ms");
+		while (++i < nth) {
+			(function () {
+				const timer2 = precise().start();
+				const record = store.search(/Lizzie Clayton/, "name");
+				timer2.stop();
+				console.log((timer2.diff() / 1000000) + "ms");
 
-	            if (!record) {
-		            console.log("Couldn't find record");
-	            }
-            }());
-        }
+				if (!record) {
+					console.log("Couldn't find record");
+				}
+			}());
+		}
 
-        second();
-    });
+		second();
+	});
 }
 
 first();
