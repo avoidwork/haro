a28282332d25dcfe01cfe98256e5c8f7386fd480
Jason Mulligan <jason.mulligan@avoidwork.com>
2025-07-13 12:07:37 -0400

    Adding new tests (WIP)

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import pluginJs from "@eslint/js";
 export default [
 	// Mocha environment for test files
 	{
-		files: ["test/**/*.js"],
+		files: ["tests/**/*.js"],
 		languageOptions: {
 			globals: {
 				...globals.mocha
